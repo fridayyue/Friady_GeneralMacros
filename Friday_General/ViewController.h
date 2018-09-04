06c21fda6819b93828ed5d1d8fab664f3085c185
岳腾飞 <yuefriday@yuetengfeideMacBook-Air.local>
2018-09-04 14:31:33 +0800

    第一提交
    创建了大体的结构和思路提交一些UI的宏

---
+++ Friday_General/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  General
+//
+//  Created by Friday on 2018/8/29.
+//  Copyright © 2018年 Friday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Friday_General/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  General
+//
+//  Created by Friday on 2018/8/29.
+//  Copyright © 2018年 Friday. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Friday_MacrosList.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    M_LogFunc(self);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
